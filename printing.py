f2f5b078b7a07ed0313157752ef3c7b5cb64aed2
alexgithui91@gmail.com <alexgithui91@gmail.com>
2022-10-16 07:46:03 +0300

    completed printing tutorial

--- printing.py
+++ printing.py
@@ -1,14 +1,33 @@
 def print_string():
-    # Print simple string
-    print("Welcome to")
+    # Printing simple string
+    print("Welcome to Coding With Hitz!")
     # Print blank line
     print("\n")
-    # Print another simple string
-    print("Coding With Hitz!")
+
+
+def print_cars():
+    # Printing Cars
+    print("Toyota")
+    print("Hummer")
+    print("Cadillac")
+    # Print blank line
+    print("\n")
+
+
+def print_with_end():
+    # Printing with end
+    print("This is", end="---")
+    print("Coding With", end="---")
+    print("Hitz", end="!!")
+    # Print blank line
+    print("\n")
+    print("Home of programming tutorials", end=":-)")
 
 
 def main():
     print_string()
+    print_cars()
+    print_with_end()
 
 
 if __name__ == "__main__":
