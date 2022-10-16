f2b71127525007992bce3a3f7e40ef02b4b48488
alexgithui91@gmail.com <alexgithui91@gmail.com>
2022-10-16 07:06:33 +0300

    print_string

---
+++ printing.py
@@ -0,0 +1,15 @@
+def print_string():
+    # Print simple string
+    print("Welcome to")
+    # Print blank line
+    print("\n")
+    # Print another simple string
+    print("Coding With Hitz!")
+
+
+def main():
+    print_string()
+
+
+if __name__ == "__main__":
+    main()
